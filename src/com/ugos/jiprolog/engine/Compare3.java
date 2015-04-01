7e5872b9d84854036005b011dbc46d8878d973b8
ugosweb <ugochirico@users.noreply.github.com>
2015-04-01 08:43:52 +0000

    Fixes to Exceptions

--- src/com/ugos/jiprolog/engine/Compare3.java
+++ src/com/ugos/jiprolog/engine/Compare3.java
@@ -24,16 +24,23 @@ import java.util.Hashtable;
 
 final class Compare3 extends BuiltIn
 {
+//	Order is neither a variable nor an atom - type_error(atom, Order)
+//	Paulo Moura: Order is an atom but not <, = or > - domain_error(order, Order)
+//	[31/03/2015 22:55:17] Paulo Moura: The "else" at line 46  should be "else if op is not an atom then type_error(atom, Order)".
+//	[31/03/2015 23:01:58] Paulo Moura: One bug left in compare. Variables, in standard order, come before other terms. Try:
+//	[31/03/2015 23:02:18 | Modificato 23:02:38] Paulo Moura: compare(Order1, 1, X).
+//	[31/03/2015 23:02:44] Paulo Moura: compare(Order2, X, 1).
+//	[31/03/2015 23:03:00 | Modificato 23:04:12] Paulo Moura: We get Order1 = Order2 = <. But it should be Order1 = > and Order2 = <.
     public final boolean unify(final Hashtable<Variable, Variable> varsTbl)
     {
         //System.out.println("compare");
-        final PrologObject oper  = getRealTerm(getParam(1));
+        final PrologObject order  = getRealTerm(getParam(1));
         final PrologObject term1 = getParam(2);
         final PrologObject term2 = getParam(3);
 
-        PrologObject oper1;
-        if (oper == null)
+        if (order == null)
         {
+            PrologObject oper1;
             if(term1.lessThen(term2))
                 oper1 = Atom.createAtom("<");
             else if(term2.lessThen(term1))
@@ -45,29 +52,23 @@ final class Compare3 extends BuiltIn
         }
         else
         {
-            final String operator = ((Atom)oper).getName();
-            if(operator.length() > 1)
-            	throw new JIPDomainException("order",Atom.createAtom(operator));
+        	if(!(order instanceof Atom))
+        		throw new JIPTypeException(JIPTypeException.ATOM, order);
 
-            try
+            final String operator = ((Atom)order).getName();
+            if(operator.length() > 1)
+            	throw new JIPDomainException("order",order);
+
+            switch(operator.charAt(0))
             {
-                final char op = ((Atom)oper).getName().charAt(0);
-                switch(op)
-                {
-                    case '<':
-                        return term1.lessThen(term2);
-                    case '>':
-                        return term2.lessThen(term1);
-                    case '=':
-                        return term1.termEquals(term2);// && !term2.lessThen(term1);
-                    default:
-                        throw  new JIPParameterTypeException(1, JIPParameterTypeException.COMPARATION_OPERATOR);
-                }
-            }
-            catch(ClassCastException ex)
-            {
-                //System.out.println(ex.toString());
-                throw  new JIPParameterTypeException(1, JIPParameterTypeException.COMPARATION_OPERATOR);
+                case '<':
+                    return term1.lessThen(term2);
+                case '>':
+                    return term2.lessThen(term1);
+                case '=':
+                    return term1.termEquals(term2);// && !term2.lessThen(term1);
+                default:
+                    throw new JIPDomainException("order",order);
             }
         }
     }
