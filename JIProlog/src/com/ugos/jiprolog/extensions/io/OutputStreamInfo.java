21e72bc0fc382ace65dca88ca9d6c338deb1eede
ugosweb <ugosweb@d14c1f26-eaaa-4e35-97e2-291d1d202067>
2015-04-07 23:06:17 +0000

    Fixes to stream options and open/3/4

--- JIProlog/src/com/ugos/jiprolog/extensions/io/OutputStreamInfo.java
+++ JIProlog/src/com/ugos/jiprolog/extensions/io/OutputStreamInfo.java
@@ -9,15 +9,31 @@ public class OutputStreamInfo extends StreamInfo
 {
     OutputStream m_stream;
 
+    public OutputStreamInfo(String name, String handle, String mode, String eof_action)
+	{
+    	super(name, handle);
+    	init(mode, eof_action);
+	}
+
     public OutputStreamInfo(String name, String handle)
 	{
     	super(name, handle);
     	init();
 	}
+
     public OutputStreamInfo(String name)
 	{
     	super(name);
     	init();
+	}
+
+    private void init(String mode, String eof_action)
+    {
+    	properties.setProperty("mode", "mode(" + mode + ")");
+//    	properties.setProperty("mode", "mode(write)");
+		properties.setProperty("output", "output");
+		properties.setProperty("reposition", "reposition(false)");
+		properties.setProperty("eof_action", "eof_action(" + eof_action + ")");
 	}
 
     private void init()
