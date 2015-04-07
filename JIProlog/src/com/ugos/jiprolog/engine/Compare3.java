b3fd0bd6136c3cd454b053459168da44ea42a161
ugosweb <ugosweb@d14c1f26-eaaa-4e35-97e2-291d1d202067>
2015-04-07 07:24:04 +0000

    Removed debug prints

--- JIProlog/src/com/ugos/jiprolog/engine/Compare3.java
+++ JIProlog/src/com/ugos/jiprolog/engine/Compare3.java
@@ -25,22 +25,11 @@ import java.util.Hashtable;
 final class Compare3 extends BuiltIn
 {
 //	Order is neither a variable nor an atom - type_error(atom, Order)
-//	Paulo Moura: Order is an atom but not <, = or > - domain_error(order, Order)
-//	[31/03/2015 22:55:17] Paulo Moura: The "else" at line 46  should be "else if op is not an atom then type_error(atom, Order)".
-//	[31/03/2015 23:01:58] Paulo Moura: One bug left in compare. Variables, in standard order, come before other terms. Try:
-//	[31/03/2015 23:02:18 | Modificato 23:02:38] Paulo Moura: compare(Order1, 1, X).
-//	[31/03/2015 23:02:44] Paulo Moura: compare(Order2, X, 1).
-//	[31/03/2015 23:03:00 | Modificato 23:04:12] Paulo Moura: We get Order1 = Order2 = <. But it should be Order1 = > and Order2 = <.
     public final boolean unify(final Hashtable<Variable, Variable> varsTbl)
     {
-        //System.out.println("compare");
         final PrologObject order  = getRealTerm(getParam(1));
         final PrologObject term1 = getParam(2);
         final PrologObject term2 = getParam(3);
-
-        System.out.println("order " + order);
-        System.out.println("term1 " + term1);
-        System.out.println("term2 " + term2);
 
         if (order == null)
         {
@@ -52,8 +41,6 @@ final class Compare3 extends BuiltIn
             else //if(term1.termEquals(term2))
                 oper1 = Atom.createAtom("=");
 //            else
-
-            System.out.println("order " + oper1);
 
             return getParam(1).unify(oper1, varsTbl);
         }
@@ -69,25 +56,13 @@ final class Compare3 extends BuiltIn
             switch(operator.charAt(0))
             {
                 case '<':
-                    if(term1.lessThen(term2))
-                    {
-                    	System.out.println("order <");
-                    	return true;
-                    }
+                    return term1.lessThen(term2);
 
                 case '>':
-                    if(term2.lessThen(term1))
-                    {
-                    	System.out.println("order >");
-                    	return true;
-                    }
+                    return term2.lessThen(term1);
 
                 case '=':
-                    if(term1.termEquals(term2)) // && !term2.lessThen(term1);
-                    {
-                    	System.out.println("order =");
-                    	return true;
-                    }
+                    return term1.termEquals(term2); // && !term2.lessThen(term1);
 
                 default:
                     throw new JIPDomainException("order",order);
