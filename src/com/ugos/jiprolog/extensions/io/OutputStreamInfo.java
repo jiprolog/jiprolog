5043bd8f04145afc3e70bfa9c05e81b12a5ef6bd
ugosweb <ugochirico@users.noreply.github.com>
2015-04-16 14:49:49 +0000

    4.0.12.3 Fixes to JIPio OutputStream for stream properties and PutByte2

--- src/com/ugos/jiprolog/extensions/io/OutputStreamInfo.java
+++ src/com/ugos/jiprolog/extensions/io/OutputStreamInfo.java
@@ -1,45 +1,20 @@
 package com.ugos.jiprolog.extensions.io;
 
 import java.io.OutputStream;
-import java.util.Enumeration;
-
-import com.ugos.io.PushBackInputStream;
 
 public class OutputStreamInfo extends StreamInfo
 {
     OutputStream m_stream;
 
-    public OutputStreamInfo(String name, String handle, String mode, String eof_action)
+    public OutputStreamInfo(String name, String handle, String mode)
 	{
     	super(name, handle);
-    	init(mode, eof_action);
+    	init(mode);
 	}
 
-    public OutputStreamInfo(String name, String handle)
-	{
-    	super(name, handle);
-    	init();
-	}
-
-    public OutputStreamInfo(String name)
-	{
-    	super(name);
-    	init();
-	}
-
-    private void init(String mode, String eof_action)
+    private void init(String mode)
     {
     	properties.setProperty("mode", "mode(" + mode + ")");
-//    	properties.setProperty("mode", "mode(write)");
-		properties.setProperty("output", "output");
-		properties.setProperty("reposition", "reposition(false)");
-		properties.setProperty("eof_action", "eof_action(" + eof_action + ")");
-	}
-
-    private void init()
-    {
-    	properties.setProperty("mode", "mode(append)");
-//    	properties.setProperty("mode", "mode(write)");
 		properties.setProperty("output", "output");
 		properties.setProperty("reposition", "reposition(false)");
 		properties.setProperty("eof_action", "eof_action(reset)");
