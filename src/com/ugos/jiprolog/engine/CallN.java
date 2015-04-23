8ff727cbe71441f12a6366f9bfa617479003bf27
ugochirico <ugos@ugosweb.com>
2015-04-23 12:40:23 +0200

    Fixes for error checking

--- src/com/ugos/jiprolog/engine/CallN.java
+++ src/com/ugos/jiprolog/engine/CallN.java
@@ -14,6 +14,8 @@ public class CallN extends BuiltIn {
 	@Override
 	public boolean unify(Hashtable<Variable, Variable> varsTbl)
 	{
+
+
 		PrologObject closure = getParam(1);
 		ConsCell params = (ConsCell)getParams().getTail();
 
@@ -22,33 +24,41 @@ public class CallN extends BuiltIn {
 			closure = ((Variable)closure).getObject();
 		}
 
-		Functor goal;
+		PrologObject goal;
 
 		if(closure instanceof Atom)
 		{
 			goal = new Functor((Atom)closure);
-			goal.setParams((ConsCell)params);
+			((Functor)goal).setParams((ConsCell)params);
 		}
 		else if(closure instanceof Functor)
 		{
 			goal = (Functor)closure;
 
-			ConsCell params1 = (ConsCell)goal.getParams().copy(true);
+			ConsCell params1 = (ConsCell)((Functor)goal).getParams().copy(true);
 
-			Functor goal1 = new Functor(goal.getName(), params1);
+			Functor goal1 = new Functor(((Functor)goal).getName(), params1);
 
 			goal.unify(goal1, varsTbl);
-
-//			if(!goal.unify(goal1, varsTbl))
-//				System.out.println("not unify");
-
 
 			ConsCell newParams = ConsCell.append(params1, (ConsCell)params);
 
 			goal1.setParams(newParams);
 
 			goal = goal1;
-//			goal.setParams(newParams);
+		}
+		else if(closure instanceof List)
+		{
+		   if(((List)closure).getHeight() + params.getHeight() > 2)
+			   throw new JIPExistenceException("procedure", (new Functor("./3", null)).getPredicateIndicator());
+		   goal = new List(closure, params);
+  	  	}
+		else if(closure instanceof ConsCell)
+		{
+		   if(((ConsCell)closure).getHeight() + params.getHeight() > 2)
+			   throw new JIPExistenceException("procedure", (new Functor("(,)/3", null)).getPredicateIndicator());
+
+		   goal = new ConsCell(closure, params);
 		}
         else if(closure == null)
 		{
@@ -59,8 +69,15 @@ public class CallN extends BuiltIn {
 			throw new JIPTypeException(JIPTypeException.CALLABLE, closure);
 		}
 
-        if(BuiltInFactory.isBuiltIn(goal.getName()))
-            goal = new BuiltInPredicate(goal);
+//		boolean succeeds = false;
+//		WAM wam = getNewWAM();
+//		succeeds = wam.query(goal);
+//
+//		wam.closeQuery();
+//
+//		return succeeds;
+//        if(BuiltInFactory.isBuiltIn(goal.getName()))
+//            goal = new BuiltInPredicate(goal);
 
         // estrae il nodo corrente
         final WAM.Node curNode = getWAM().getCurNode();
@@ -75,6 +92,13 @@ public class CallN extends BuiltIn {
 		return false;
 	}
 
-
+	private final WAM getNewWAM()
+    {
+		WAM wam = getWAM();
+        if(wam instanceof WAMTrace)
+            return new WAMTrace((WAMTrace)wam);
+        else
+            return new WAM(wam);
+    }
 
 }
