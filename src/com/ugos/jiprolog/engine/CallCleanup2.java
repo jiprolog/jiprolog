0aadb9dc2ba07b5d5715c62d2f2a110a4a598d79
ugochirico <ugos@ugosweb.com>
2015-05-11 07:14:47 +0200

    Missed CallCleanup2.java

---
+++ src/com/ugos/jiprolog/engine/CallCleanup2.java
@@ -0,0 +1,91 @@
+package com.ugos.jiprolog.engine;
+
+import java.util.Hashtable;
+
+public class CallCleanup2 extends Call1 {
+
+	WAM wam;
+	private PrologObject m_goal;
+	private PrologObject m_cleanupGoal;
+
+	@Override
+	public boolean unify(Hashtable<Variable, Variable> varsTbl) {
+
+		if(m_goal == null)
+		{
+			PrologObject goal1 = getParam(1);
+			if(goal1 instanceof Variable)
+			{
+				goal1 = ((Variable)goal1).getObject();
+			}
+
+			m_goal = getGoal(goal1).copy(true);
+		}
+
+		if(m_cleanupGoal == null)
+		{
+			PrologObject goal1 = getParam(2);
+			if(goal1 instanceof Variable)
+			{
+				goal1 = ((Variable)goal1).getObject();
+			}
+
+			m_cleanupGoal = getGoal(goal1);
+		}
+
+		boolean solution;
+
+		if(wam == null)
+		{
+			wam = getNewWAM();
+			try
+			{
+				solution = wam.query(m_goal);
+				if(solution)
+				{
+					getParam(1).unify(m_goal, varsTbl);
+
+					if(wam.hasMoreChoicePoints())
+						return true;
+				}
+			}
+			catch(JIPRuntimeException ex)
+			{
+			}
+		}
+		else
+		{
+			try
+			{
+				solution = wam.nextSolution();
+				if(solution)
+				{
+					getParam(1).unify(m_goal, varsTbl);
+					if(wam.hasMoreChoicePoints())
+						return true;
+				}
+
+			}
+			catch(JIPRuntimeException ex)
+			{
+			}
+		}
+
+		callCleanup();
+
+		return true;
+	}
+
+	private void callCleanup()
+	{
+		wam.closeQuery();
+        final WAM.Node curNode = getWAM().getCurNode();
+        curNode.m_altBody = new ConsCell(new Functor("once/1", new ConsCell(m_cleanupGoal, null)), null);
+	}
+
+	@Override
+	public boolean hasMoreChoicePoints()
+	{
+		return wam != null && !wam.isClosed();//true;//m_goal1 == null || m_goal2 == null;
+	}
+}
